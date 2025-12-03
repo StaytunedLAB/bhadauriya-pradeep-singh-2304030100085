f53a6094af2ca11b33f5e75122ef67cdf2e8f4a3
BHADAURIYA PRADIPSINGH MANOJSINGH <bhadauriyapradeep290@gmail.com>
2025-12-03 13:48:37 +0530

    part 1 basic

---
+++ src/Part 1/Basic/01-hello-world.js
@@ -0,0 +1,8 @@
+/****************************************************
+ * JavaScript.info – Part 1
+ * Chapter 01: Hello World
+ * Description: Basic console output
+ ****************************************************/
+
+console.log("Hello JavaScript!")
+
---
+++ src/Part 1/Basic/05-operators.js
@@ -0,0 +1,9 @@
+/****************************************************
+ * Chapter 05: Operators
+ * Description: arithmetic + assignment + power
+ ****************************************************/
+
+let a = 10, b = 3
+console.log(a + b, a - b, a * b, a / b, a % b, a ** b)
+a += 5
+console.log("a after += 5:", a)
