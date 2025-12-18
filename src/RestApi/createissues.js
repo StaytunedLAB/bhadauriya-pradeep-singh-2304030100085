2f2e72236f374ca5ddecd9d80945415d88ea9d0d
BHADAURIYA PRADIPSINGH MANOJSINGH <bhadauriyapradeep290@gmail.com>
2025-12-18 11:26:54 +0530

    add files

---
+++ src/RestApi/createissues.js
@@ -0,0 +1,26 @@
+import { Octokit } from "@octokit/core";
+
+
+const octokit = new Octokit({
+    auth: "TOKEN-KEY"
+});
+
+async function createIssue () {
+    try {
+        await octokit.request('POST /repos/StaytunedLAB/bhadauriya-pradeep-singh-2304030100085/issues', {
+            owner: 'StaytunedLAB',
+            repo: 'bhadauriya-pradeep-singh-2304030100085',
+            title: 'issue created using js code',
+            body: "issue created using javascript code using rest api key token.",
+            labels: ['Task'],
+            headers: {
+                'X-GitHub-Api-Version': '2022-11-28'
+            }
+        });
+        console.log("issue created");
+    } catch (error) {
+        console.error("Error creating issue:", error);
+    }
+}
+createIssue(
+);
